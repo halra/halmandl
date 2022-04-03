83611f1ba682e70e4203c84a6e167f51ab83a22b
halra <rarichn@gmail.com>
2022-04-03 21:08:56 +0200

    Updated tests and no header fix

--- main.go
+++ main.go
@@ -21,30 +21,35 @@ type FileStats struct {
 	StartedAt       time.Time
 	Junk            int64
 	Parts           int
-	junkSize        int
+	junkSize        int64
 	ConcurrentParts int64
 	PercDone        float32
 	LastMeassured   int64
 	BytesPerSecond  float32
+	CompletedJunks  int64
 }
 
-func CDownload(dir string, url string) {
+type Options struct {
+	JunkSize        int64
+	ConcurrentParts int64
+	UseStats        bool
+}
 
-	stats := &FileStats{} // TODO  build this at the end in a one liner
+func CDownload(dir string, url string, options Options) {
 
 	//Params
-	concurrentParts := int64(6) // allow big files ...
-	junkSize := 4194304         //each junk in bytes
+	if options.ConcurrentParts == 0 {
+		options.ConcurrentParts = int64(6) // allow big files ...
+	}
+	if options.JunkSize == 0 {
+		options.JunkSize = int64(4194304) // allow big files ...
+	}
 	//End Params
-	stats.ConcurrentParts = concurrentParts
-	stats.junkSize = junkSize
 
 	var wg sync.WaitGroup
 
 	_, file := path.Split(url) // filename
 	filepath := path.Join(dir, file)
-	stats.Filename = file
-	stats.TargetDirectory = filepath
 
 	err := os.MkdirAll(dir, os.ModePerm) // create path if not exist
 	if err != nil {
@@ -57,36 +62,35 @@ func CDownload(dir string, url string) {
 		return
 	}
 	defer f.Close()
-
-	res, _ := http.Head(url) // fetch required headers, we decide on the header if we use junks
-	length, _ := strconv.ParseInt(res.Header.Get("Content-Length"), 10, 64)
-	acceptRange := res.Header.Get("Accept-Ranges") // check if we can use a concurrent strategie
-	fmt.Println("Accept-Ranges", acceptRange)
+	length := int64(1)
 	limit := int64(1)
-	if strings.Contains(acceptRange, "bytes") {
-		limit = length / int64(junkSize) // split in junks of n bytes
-		if limit == 0 {                  // handle small files, files < junksize
-			limit = 1
+	//TODO some servers wont respond here, we could peek with a range ret to read the headers
+	res, err := http.Head(url) // fetch required headers, we decide on the header if we use junks
+	if err == nil {            // Only do if header is present
+		//fmt.Println(err)
+		contentLengthHeader := res.Header.Get("Content-Length")
+		if contentLengthHeader != "" {
+			length, _ = strconv.ParseInt(res.Header.Get("Content-Length"), 10, 64)
+		}
+		acceptRange := res.Header.Get("Accept-Ranges") // check if we can use a concurrent strategie
+		fmt.Println("Accept-Ranges", acceptRange)
+		if strings.Contains(acceptRange, "bytes") {
+			limit = length / int64(options.JunkSize) // split in junks of n bytes
+			if limit == 0 {                          // handle small files, files < junksize
+				limit = 1
+			}
 		}
 	}
-	lenJunk := length / limit // Bytes for each Go-routine
-	diff := length % limit    //  the remaining for the last junk
-
-	//stat HELPER
-	stats.Parts = int(limit)
-	stats.StartedAt = time.Now()
-	stats.Size = length
-
-	//END stat HELPER
-
-	guard := make(chan struct{}, concurrentParts) // semaphore for concurrent requests
+	lenJunk := length / limit                             // Bytes for each Go-routine
+	diff := length % limit                                //  the remaining for the last junk
+	guard := make(chan struct{}, options.ConcurrentParts) // semaphore for concurrent requests
 	defer func() {
 		close(guard)
 	}()
 
-	//this kinda fails on junks ..., consider whole time bytes/time -> first meassure
-	stats.LastMeassured = time.Now().UnixNano()
-
+	//Init stats
+	stats := &FileStats{Transfered: 0, Filename: file, Parts: int(limit), StartedAt: time.Now(), Size: length,
+		TargetDirectory: filepath, ConcurrentParts: options.ConcurrentParts, junkSize: options.JunkSize, CompletedJunks: int64(0)}
 	for i := int64(0); i < limit; i++ {
 
 		guard <- struct{}{} // add semaphore
@@ -110,8 +114,10 @@ func CDownload(dir string, url string) {
 			stats.Junk = i
 			client := &http.Client{}
 			req, _ := http.NewRequest("GET", url, nil)
-			range_header := "bytes=" + strconv.FormatInt(min, 10) + "-" + strconv.FormatInt(max, 10) // add header for junk size
-			req.Header.Add("Range", range_header)
+			if limit > 1 { // only had
+				rangeHeader := "bytes=" + strconv.FormatInt(min, 10) + "-" + strconv.FormatInt(max, 10) // add header for junk size
+				req.Header.Add("Range", rangeHeader)
+			}
 			resp, err := client.Do(req)
 
 			if err != nil {
@@ -122,6 +128,9 @@ func CDownload(dir string, url string) {
 
 			//just download normaly
 			if limit == 1 {
+				if options.UseStats {
+					go doStats(stats, false)
+				}
 				_, err = io.Copy(f, resp.Body)
 				if err != nil {
 					fmt.Println(err)
@@ -130,7 +139,9 @@ func CDownload(dir string, url string) {
 				var reader []byte
 				reader, _ = ioutil.ReadAll(resp.Body)
 				stats.Transfered = stats.Transfered + int64(len(reader))
-				go test(stats)
+				if options.UseStats {
+					go doStats(stats, true)
+				}
 				_, err = f.WriteAt(reader, int64(min))
 				if err != nil {
 					fmt.Println(err)
@@ -140,18 +151,17 @@ func CDownload(dir string, url string) {
 		}(min, max, i)
 	}
 	wg.Wait()
-
 }
 
 // TODO write a stat struct and pass as channel
-func test(f *FileStats) {
+func doStats(f *FileStats, hasJunks bool) {
 	//fmt.Println(f)
-	divider := (time.Now().UnixNano() - f.LastMeassured) / time.Second.Nanoseconds()
+	divider := ((time.Now().UnixMilli() / 1000) - f.StartedAt.Unix())
 	if divider == 0 {
 		divider = 1
 	}
 	f.BytesPerSecond = float32(int64(f.junkSize)) / float32(divider)
 	f.PercDone = float32(f.Transfered) / float32(f.Size)
-	//fmt.Printf("%+v\n", f.BytesPerSecond)
+	fmt.Printf("%+v\n", f)
 	f.LastMeassured = time.Now().UnixNano()
 }
--- main_test.go
+++ main_test.go
@@ -17,23 +17,54 @@ import (
 var mockString string
 
 func TestCDownloadWithRange(t *testing.T) {
-	mockString = RandStringRunes(123456789)
-	CDownload("/", testServer.URL+"/foo.txt")
-}
 
-func TestCDownloadNoRange(t *testing.T) {
+	op := Options{JunkSize: 4194304, ConcurrentParts: 10, UseStats: true}
+
 	mockString = RandStringRunes(123456789)
 	origHash := getSha1([]byte(mockString))
 	fmt.Println(origHash)
-	CDownload("/", testServerNoRange.URL+"/bar.txt")
-	data, _ := ioutil.ReadFile("/bar.txt")
+	CDownload("/", testServer.URL+"/foo.txt", op)
+	data, _ := ioutil.ReadFile("/\\foo.txt")
+	doHash := getSha1(data)
+	fmt.Println(doHash)
+	if !strings.EqualFold(origHash, doHash) {
+		t.Logf("TestCDownloadWithRange failed\n")
+		t.Fail()
+	}
+}
+
+func TestCDownloadWithRangeBrokenServer(t *testing.T) {
+
+	op := Options{JunkSize: 4194304, ConcurrentParts: 10, UseStats: true}
+
+	mockString = RandStringRunes(123456789)
+	origHash := getSha1([]byte(mockString))
+	fmt.Println(origHash)
+	CDownload("/", testServerBroken.URL+"/foo2.txt", op)
+	data, _ := ioutil.ReadFile("/\\foo2.txt")
+	doHash := getSha1(data)
+	fmt.Println(doHash)
+	if strings.EqualFold(origHash, doHash) {
+		t.Logf("TestCDownloadWithRange failed\n")
+		t.Fail()
+	}
+}
+
+func TestCDownloadNoRange(t *testing.T) {
+	op := Options{JunkSize: 5, ConcurrentParts: 5, UseStats: true}
+	mockString = RandStringRunes(123456789)
+	origHash := getSha1([]byte(mockString))
+	fmt.Println(origHash)
+	CDownload("/", testServerNoRange.URL+"/bar.txt", op)
+	data, _ := ioutil.ReadFile("/\\bar.txt")
 	doHash := getSha1(data)
 	fmt.Println(doHash)
 }
 
 func TestCDownloadSmallFiles(t *testing.T) {
+	op := Options{JunkSize: 5, ConcurrentParts: 5, UseStats: true}
 	mockString = RandStringRunes(1)
-	CDownload("/", testServer.URL+"/foo1.txt")
+	CDownload("/", testServer.URL+"/foo1.txt", op)
 }
 
 var testServer = httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
@@ -46,7 +77,27 @@ var testServer = httptest.NewServer(http.HandlerFunc(func(res http.ResponseWrite
 	if len(splitted) == 2 {
 		min, _ := strconv.ParseInt(splitted[0], 10, 64)
 		max, _ := strconv.ParseInt(splitted[1], 10, 64)
-		res.Write([]byte(mockString)[min : max+1]) // +1, we go from min till < max
+		res.Write([]byte(mockString)[min:max])
+	} else {
+		res.Write([]byte(mockString))
+	}
+
+}))
+
+var testServerBroken = httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+	ra := req.Header.Get("Range")
+	trimmed := strings.TrimPrefix(ra, "bytes=")
+	splitted := strings.Split(trimmed, "-")
+	res.Header().Add("Content-Length", strconv.Itoa(len(mockString)))
+	res.Header().Add("Accept-Ranges", "bytes")
+
+	if len(splitted) == 2 {
+		min, _ := strconv.ParseInt(splitted[0], 10, 64)
+		if min > 4194304 && min < 4194304*3 {
+			return
+		}
+		max, _ := strconv.ParseInt(splitted[1], 10, 64)
+		res.Write([]byte(mockString)[min:max])
 	} else {
 		res.Write([]byte(mockString))
 	}
